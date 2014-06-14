84cdb03c0360d186318cfcb3a91689695536c3ac
Dave Willkomm <david.willkomm@fidelitylife.com>
2014-06-14 05:41:35 -0500

    Implemented failing test
    Gitlab Merge Request Builder 1.2.3 introduced a logic bug whereby merge
    requests already processed by the plugin whose Jenkins user comments had
    been deleted would not trigger another build, even when a trigger comment
    was added. (Deleting all Jenkins user comments from a MR used to be a
    workaround to retrigger a build before the trigger comment feature was
    introduced - thanks @vbyjsue!)
    
    This bug is demonstrated by the failing builds_givenATriggerCommentAndAReachableLatestCommit.
    Additional GitlabMergeRequestWrapper.check() tests were implemented in order
    to help ensure that the next commit's bug fix wouldn't break other things.
    
    pom.xml
    * Added XML declaration
    * Corrected <project>
    * Added JMockit dependency

---
+++ src/test/java/com/jenkinsci/plugins/gitlab/GitlabMergeRequestWrapper_check_Test.java
@@ -0,0 +1,174 @@
+package com.jenkinsci.plugins.gitlab;
+
+import hudson.util.Secret;
+import mockit.Expectations;
+import mockit.Injectable;
+import mockit.Mock;
+import mockit.MockUp;
+import mockit.NonStrictExpectations;
+import mockit.Tested;
+import mockit.Verifications;
+import mockit.integration.junit4.JMockit;
+import org.gitlab.api.GitlabAPI;
+import org.gitlab.api.models.GitlabCommit;
+import org.gitlab.api.models.GitlabMergeRequest;
+import org.gitlab.api.models.GitlabNote;
+import org.gitlab.api.models.GitlabProject;
+import org.gitlab.api.models.GitlabUser;
+import org.jenkinsci.plugins.gitlab.GitlabBuildTrigger;
+import org.jenkinsci.plugins.gitlab.GitlabMergeRequestBuilder;
+import org.jenkinsci.plugins.gitlab.GitlabMergeRequestStatus;
+import org.jenkinsci.plugins.gitlab.GitlabMergeRequestWrapper;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import static mockit.Deencapsulation.getField;
+import static mockit.Deencapsulation.invoke;
+import static mockit.Deencapsulation.setField;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(JMockit.class)
+public class GitlabMergeRequestWrapper_check_Test {
+    @Injectable GitlabAPI api;
+    @Injectable GitlabBuildTrigger trigger;
+    @Injectable GitlabMergeRequest mergeRequest;
+    @Injectable GitlabMergeRequestBuilder builder;
+    @Injectable GitlabProject project;
+    @Tested GitlabMergeRequestWrapper subject;
+    DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-DD");
+    GitlabCommit commit = new GitlabCommit();
+    GitlabNote jenkinsNote = new GitlabNote();
+    GitlabNote triggerNote = new GitlabNote();
+    GitlabUser botUser = new GitlabUser();
+
+    // Mock dependencies used during static initialization.
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        new MockUp<GitlabBuildTrigger.GitlabBuildTriggerDescriptor>() {
+            @Mock void load() {}
+        };
+
+        new MockUp<Secret>() {
+            @Mock Secret fromString(String data) { return null; }
+        };
+    }
+
+    @Before
+    public void before() throws Exception {
+        new MockUp<GitlabBuildTrigger.GitlabBuildTriggerDescriptor>() {
+            @Mock String getBotUsername() { return "test-bot-username"; }
+        };
+
+        new NonStrictExpectations() {{
+            api.getCommits(mergeRequest); result = Arrays.asList(commit);
+            trigger.getAssigneeFilter(); result = "";
+            trigger.getTriggerComment(); result = "test-trigger-comment";
+        }};
+
+        botUser.setUsername("test-bot-username");
+        commit.setId("test-commit-id");
+        jenkinsNote.setAuthor(botUser);
+        jenkinsNote.setBody("test-jenkins-note");
+        jenkinsNote.setCreatedAt(dateFormat.parse("2015-01-01"));
+        triggerNote.setBody("test-trigger-comment");
+        triggerNote.setCreatedAt(dateFormat.parse("2015-01-02"));
+    }
+
+    // This test asserts the default givens for this suite. Individual test methods will identify when they change this.
+    @Test
+    public void given_noJenkinsNote_noLastNote_latestCommitIsNotReached() throws Exception {
+        assertNull(invoke(subject, "getJenkinsNote", mergeRequest, api));
+        assertNull(invoke(subject, "getLastNote", mergeRequest, api));
+        assertTrue((boolean) invoke(subject, "latestCommitIsNotReached", commit));
+    }
+
+    @Test
+    public void builds() throws Exception {
+        subject.check(mergeRequest);
+
+        new Verifications() {{
+            invoke(subject, "build", new HashMap<String, String>());
+        }};
+    }
+
+    @Test
+    public void builds_givenAJenkinsNote() throws Exception {
+        new Expectations() {{
+            api.getAllNotes(mergeRequest); result = Arrays.asList(jenkinsNote);
+        }};
+
+        assertEquals(invoke(subject, "getJenkinsNote", mergeRequest, api), jenkinsNote);
+
+        subject.check(mergeRequest);
+
+        new Verifications() {{
+            invoke(subject, "build", new HashMap<String, String>());
+        }};
+    }
+
+    @Test
+    public void doesNotBuild_givenAJenkinsNoteAndAReachableLatestCommit() throws Exception {
+        new MockUp<GitlabMergeRequestWrapper>() {
+            @Mock(invocations = 0) void build(Map m) {}
+        };
+
+        new Expectations() {{
+            api.getAllNotes(mergeRequest); result = Arrays.asList(jenkinsNote);
+        }};
+
+        ((GitlabMergeRequestStatus) getField(subject, "mergeRequestStatus")).setLatestCommitOfMergeRequest(
+                mergeRequest.getId().toString(),
+                commit.getId());
+
+        assertEquals(invoke(subject, "getJenkinsNote", mergeRequest, api), jenkinsNote);
+        assertFalse((boolean) invoke(subject, "latestCommitIsNotReached", commit));
+
+        subject.check(mergeRequest);
+    }
+
+    @Test
+    public void doesNotBuild_givenAReachableLatestCommit() throws Exception {
+        new MockUp<GitlabMergeRequestWrapper>() {
+            @Mock(invocations = 0) void build(Map m) {}
+        };
+
+        setAReachableLatestCommit();
+
+        subject.check(mergeRequest);
+    }
+
+    @Test
+    public void builds_givenATriggerCommentAndAReachableLatestCommit() throws Exception {
+        new Expectations() {{
+            api.getAllNotes(mergeRequest); result = Arrays.asList(triggerNote);
+        }};
+
+        setAReachableLatestCommit();
+
+        assertEquals(invoke(subject, "getLastNote", mergeRequest, api), triggerNote);
+        assertFalse((boolean) invoke(subject, "latestCommitIsNotReached", commit));
+
+        subject.check(mergeRequest);
+
+        new Verifications() {{
+            invoke(subject, "build", new HashMap<String, String>());
+        }};
+    }
+
+    private void setAReachableLatestCommit() {
+        ((GitlabMergeRequestStatus) getField(subject, "mergeRequestStatus")).setLatestCommitOfMergeRequest(
+                mergeRequest.getId().toString(),
+                commit.getId());
+    }
+}
